2438df5629c8cb9369f44ee76cf52319253a3e14
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2022-05-09 08:01:22 +1000

    Added start-up

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const config = require('./config.json');
 const electron = require('electron');
 const { app } = electron;
 const { protocol } = electron;
+const { ipcMain } = electron;
 
 
 const BrowserWindow = electron.BrowserWindow;
@@ -14,6 +15,8 @@ const path = require('path');
 const url = require('url');
 const fs = require('fs');
 const os = require('os');
+const https = require('https');
+var JSZip = require("jszip");
 
 var mainWindow = null;
 
@@ -34,7 +37,8 @@ function createWindow() {
             nodeIntegration: false,
             contextIsolation: true,
             enableRemoteModule: false,
-            webSecurity: false
+            webSecurity: false,
+            preload: path.join(__dirname, "preload.js")
         }
 
     });
@@ -44,7 +48,7 @@ function createWindow() {
     }
 
     mainWindow.setMenu(null);
-    mainWindow.setTitle('Dr Neil\'s Stuff') // Window name isn't this
+    mainWindow.setTitle('Dr Neil\'s Stuff');
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'html',
@@ -73,5 +77,144 @@ app.on('ready', () => {
     });
 
     createWindow();
+
+});
+
+
+ipcMain.on('retrieve', function(event, arg) {
+    var data = [];
+    var zipfile = path.basename(new URL(arg).pathname);
+    var content = "";
+
+    var request = https.get(url.parse(arg), function(response) {
+        response.on('data', (chunk) => {
+            data.push(chunk);
+        });
+
+        response.on('end', function() {
+            function getContent(zip) {
+
+                return new Promise(async(accept, reject) => {
+
+                    zip.forEach(async function(relativePath, zipEntry) {
+
+                        if (zipEntry.name.endsWith("manifest.json")) {
+                            content = await zipEntry.async("string");
+                            accept(content);
+                        }
+
+                    })
+
+                });
+
+            }
+
+            var buffer = Buffer.concat(data);
+
+            JSZip.loadAsync(buffer).then(async function(zip) {
+                var dir = path.join(__dirname, 'packages');
+
+                fs.mkdirSync(dir, {
+
+                    recursive: true
+                }, (err) => {
+                    if (err) {
+                        event.sender.send('retrieve-complete', err);
+                        console.log("error occurred in creating new directory", err);
+                        return;
+                    }
+                });
+
+                var content = await getContent(zip);
+
+                fs.writeFile(path.join(dir, zipfile), buffer, "binary", function(err) {
+
+                    if (err) {
+                        console.log(err);
+                    }
+
+                });
+
+                event.sender.send('retrieve-complete', content);
+
+            }).then(function(text) {});
+
+        });
+
+    }).on('error', function(err) {
+        console.log(err.message);
+        event.sender.send('retrieve-complete', err.message);
+    });
+
+});
+
+ipcMain.on('load', async function(event, arg) {
+    function processZip(data) {
+
+        return new Promise(async(accept, reject) => {
+            var zip = new JSZip();
+            var manifest = {};
+
+            zip.loadAsync(data).then(async function(zip) {
+
+                var files = zip.filter(function(relativePath, file) {
+                    return relativePath.endsWith(".manifest/manifest.json");
+                });
+
+                var content = await files[0].async("string");
+
+                manifest = JSON.parse(content);
+
+            }).then(function() {
+                accept(manifest);
+            });
+
+        });
+
+    }
+
+    var dir = path.join(__dirname, 'packages');
+    const files = await fs.promises.readdir(dir);
+
+    var manifests = [];
+
+    for (const file of files) {
+        function readZip(filename) {
+            return new Promise(async(accept, reject) => {
+
+                fs.readFile(filename, async function(err, data) {
+
+                    if (err) {
+
+                        reject(err);
+                    }
+
+                    var manifest = await processZip(data);
+
+                    manifest["filename"] = filename;
+                    manifest["base"] = file.replace(".zip", "");
+
+
+                    accept(manifest);
+
+                });
+
+            });
+
+        }
+
+        var filename = path.join(__dirname, 'packages', file);
+        console.log("Loading...", filename);
+
+        var manifest = await readZip(filename)
+
+        manifests.push(manifest);
+
+    }
+
+    console.log("Load Completed...", JSON.stringify(manifests));
+
+
+    event.sender.send('load-complete', JSON.stringify(manifests));
 
 });
---
+++ preload.js
@@ -0,0 +1,28 @@
+const {
+    contextBridge,
+    ipcRenderer
+} = require("electron");
+
+const fs = require('fs');
+const os = require('os');
+
+contextBridge.exposeInMainWorld(
+    "api", {
+        load: () => {
+            return ipcRenderer.send('load');
+        },
+        retrieve: (url) => {
+            return ipcRenderer.send('retrieve', url);
+        },
+        on: (channel, callback) => {
+            ipcRenderer.on(channel, (event, args) => {
+                callback(event, args);
+            });
+        },
+        log: (message) => {
+            console.log(message);
+        }
+
+    }
+
+);
